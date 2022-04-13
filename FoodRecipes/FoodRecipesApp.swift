52941dc350e94aca902c95ca97cdd037945ba4ed
Tiger <thisisstreamliner@gmail.com>
2022-04-13 22:15:47 +0300

    Initial Commit

---
+++ FoodRecipes/FoodRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodRecipesApp.swift
+//  FoodRecipes
+//
+//  Created by Tiger Mei on 13.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct FoodRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
