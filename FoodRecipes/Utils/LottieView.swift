55732b7a22dc2a24f644329fe1f44a9554d9cea9
Tiger <thisisstreamliner@gmail.com>
2022-04-14 00:31:58 +0300

    Add LottieView

---
+++ FoodRecipes/Utils/LottieView.swift
@@ -0,0 +1,36 @@
+//
+//  LottieView.swift
+//  FoodRecipes
+//
+//  Created by Tiger Mei on 14.04.2022.
+//
+
+import Lottie
+import SwiftUI
+
+struct LottieView: UIViewRepresentable {
+    
+    typealias UIViewType = UIView
+    var name = ""
+    var loopMode: LottieLoopMode = .loop
+    
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView(frame: .zero)
+        let animationView = AnimationView()
+        animationView.animation = Animation.named(name)
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = loopMode
+        animationView.play()
+        
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(animationView)
+        NSLayoutConstraint.activate([
+            animationView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            animationView.widthAnchor.constraint(equalTo: view.widthAnchor)
+        ])
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIView, context: Context) {  }
+    
+}
